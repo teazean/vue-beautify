83bcc8df0429c62c0e41c0e2765e8b9016ba6a4e
teazean <zhanglei55@zhanglei-mac.local>
2016-05-31 14:12:41 +0800

    fixed closure

--- src/vue-beautify.js
+++ src/vue-beautify.js
@@ -2,23 +2,18 @@ var beautify = require('js-beautify');
 
 var templateReg = /(<\/?)template([\s\S]*?>)/ig;
 var vueBeautifyDivReg = /(<\/?)vueBeautifyDiv([\s\S]*?>)/ig;
-var oldHtmlBeautify = beautify.html;
-
-beautify.html = function (text, options) {
-	text = text.replace(templateReg, function(match, begin, end) {
-		return begin + 'vueBeautifyDiv' + end;
-	});
-	text = oldHtmlBeautify(text, options);
-	return text.replace(vueBeautifyDivReg, function(match, begin, end) {
-		return begin + 'template' + end;
-	});
-}
 
 function beautifyVue (text, options) {
 	if (!text) {
 		return;
 	}
-	return beautify['html'](text, options);
+	text = text.replace(templateReg, function(match, begin, end) {
+		return begin + 'vueBeautifyDiv' + end;
+	});
+	text = beautify.html(text, options);
+	return text.replace(vueBeautifyDivReg, function(match, begin, end) {
+		return begin + 'template' + end;
+	});
 }
 
 module.exports = beautifyVue;
